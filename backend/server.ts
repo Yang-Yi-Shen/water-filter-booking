9ec7b11ad58d2ad56e3c4edab57ddd29bc9bbd95
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-01-09 16:53:00 +0800

    feat: add /getearnings api

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import { getBookings } from './booking'
+import { getEarnings } from './earnings'
 
 const app = express()
 const PORT = 6900
@@ -8,6 +9,13 @@ const PORT = 6900
 // GET APIs
 app.get('/getBookings', async (req, res) => {
     const response = await getBookings()
+    res.json({
+        data: response
+    })
+})
+
+app.get('/getEarnings', async (req, res) => {
+    const response = await getEarnings()
     res.json({
         data: response
     })
