2ae3626e5727a7fc3d2fa6734f522a853b70c9ab
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-01-09 16:51:33 +0800

    feat: added /getbookings api

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,17 @@
 import express from 'express'
 
+import { getBookings } from './booking'
+
 const app = express()
 const PORT = 6900
+
+// GET APIs
+app.get('/getBookings', async (req, res) => {
+    const response = await getBookings()
+    res.json({
+        data: response
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`app listening at port ${PORT}`)
