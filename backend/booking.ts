38e1d755b1e636ed684fb55d10c2e3c93a1dd7d2
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-01-09 15:48:58 +0800

    feat: create make booking function

--- backend/booking.ts
+++ backend/booking.ts
@@ -1,5 +1,37 @@
+import sqlite3 from "sqlite3";
+
 export interface Booking {
-    tsart: Date;
-    tend: Date;
-    revenue: number;
+    tsart: number; // dates are stored in milliseconds so datatype is number
+    tend: number;
+    revenue: number; // revenue is calculated depending on services ordered
+}
+
+// look into combining this & LoginResponse into one DBResponse
+export interface MakeBookingResponse {
+    success: boolean;
+}
+
+export function makeBooking(
+    tsart: number,
+    tend: number,
+    revenue: number
+): Promise<MakeBookingResponse> {
+    return new Promise<MakeBookingResponse>((resolve, reject) => {
+        let db = new sqlite3.Database('./database.db')
+
+        db.run('INSERT INTO bookings VALUES(?, ?, ?)', [tsart, tend, revenue], (err) => {
+            if (err) {
+                console.error(err)
+                reject({
+                    success: false
+                })
+                return
+            }
+
+            db.close()
+            resolve({
+                success: true
+            })
+        })
+    })
 }
