aa053d6ae888adce1f8b1b7e07718afa0a399383
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-01-09 17:00:47 +0800

    feat: add /userlogin api

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 
 import { getBookings } from './booking'
 import { getEarnings } from './earnings'
+import { userLogin } from './auth'
 
 const app = express()
 const PORT = 6900
@@ -16,6 +17,15 @@ app.get('/getBookings', async (req, res) => {
 
 app.get('/getEarnings', async (req, res) => {
     const response = await getEarnings()
+    res.json({
+        data: response
+    })
+})
+
+// POST APIs
+app.post('/userLogin', async (req, res) => {
+    const password = req.body.password
+    const response = await userLogin(password)
     res.json({
         data: response
     })
